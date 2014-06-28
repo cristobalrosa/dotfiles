a76963955cff5cadf09680b6f01bc6c0a73502b9
tty1sr <tty1sr@yahoo.es>
2014-06-28 12:54:31 +0200

    Added my gitconfig configuration and the globalgitignore

--- links.sh
+++ links.sh
@@ -2,4 +2,6 @@
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/bash_functions ~/.bash_functions
 ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
 
